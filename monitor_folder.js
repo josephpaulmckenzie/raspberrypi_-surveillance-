8dd76d49dc12bfc32cfaa774edcacf660fdb72f8
Joseph Mckenzie <joseph.p.mckenzie84@gmail.com>
2023-07-13 15:20:30 -0400

    Added tests and updated functions for uploading video event to s3

--- monitor_folder.js
+++ monitor_folder.js
@@ -1,33 +1,36 @@
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
 
-const folderPath = '/home/josephmckenzie/videos';
 
-
-// Watches for events in the directory (New files and Deletion of existing files)
 const monitorFolder = (folderPath) => {
+
+  const folderPath = process.env.FILE_PATH;
+
   fs.watch(folderPath, (eventType, filename) => {
     if (filename) {
       const filePath = path.join(folderPath, filename);
-        fs.stat(filePath, (err, stats) => {
-            if (err) {
-                if (eventType === 'rename' && err.code === 'ENOENT') {
-                    console.log(`File deleted: ${filename}`);
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          if (eventType === 'rename' && err.code === 'ENOENT') {
+            // If file is deleted send an alert to either a text or as a push notification in an app
+            console.log(`File deleted: ${filename}`);
 
-                } else {
-                    console.error('Error:', err);
-            }
-            return;
-            }
-
-            if (eventType === 'rename') {
-                if (stats.isFile()) {
-                    console.log(`New file added: ${filename}`);
-                
-            }
-            }
-        });
+          } else {
+            // Log error to csv filed that is stored locally as well as a file that's stored in S3
+            console.error('Error:', err);
+          }
+          return;
         }
+        // Log all new files added to dynamo db or a simple csv in a s3 bucket 
+        if (eventType === 'rename') {
+          if (stats.isFile()) {
+            console.log(`New file added: ${filename}`);
+
+          }
+        }
+      });
+    }
   });
 };
 
